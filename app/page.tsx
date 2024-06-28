9c71bbc25020ddeafa2e8a1a2993c11bb9f8254c
VishalyGinny <vishalyginny@gmail.com>
2024-06-28 11:14:38 +0800

    theme added

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="text-3xl font-bold">Hello Home!</div>
-      <div className="text-xl font-medium ">This is line two.</div>
+    <div className="flex min-h-screen flex-col items-center">
+      <div className="text-3xl font-bold">
+        Hey everyone, this is my first website!{" "}
+      </div>
+      <div className="text-xl font-medium ">My name is Vishaly Ginny.</div>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { ModeToggle } from "@/components/ModeToggle";
 
 export const Navbar = () => {
   return (
-    <div>
-      <Button asChild variant="link">
-        <Link href="/">Home</Link>
-      </Button>
-      <Button asChild variant="link">
-        <Link href="/contact" className="">
-          Contact
-        </Link>
-      </Button>
-      <Button asChild variant="link">
-        <Link href="/about">About</Link>
-      </Button>
+    <div className="flex justify-between items-center m-2">
+      <div className="flex space-x-4">
+        <Button asChild variant="link">
+          <Link href="/">Home</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/contact">Contact</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/about">About</Link>
+        </Button>
+      </div>
+      <ModeToggle />
     </div>
   );
 };
