befefd0424211d8047dd7fb96aa0ec9d42cf0d8a
VishalyGinny <vishalyginny@gmail.com>
2024-07-02 12:01:12 +0800

    image is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import whiteforest from "../public/white-forest.jpg";
+import chemelion from "../public/chemelion.png";
+
 export default function Home() {
   return (
     <section className="w-full min-h-screen py-12 md:py-24 lg:py-32 xl:py-48 ">
@@ -10,6 +13,8 @@ export default function Home() {
               <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                 Hey everyone, this is my first website!
               </h1>
+              {/* Adding the image here */}
+
               <p className="max-w-[600px] text-muted-foreground md:text-xl">
                 I am a front end developer and Data Analyst. I love to build
                 websites.
@@ -28,6 +33,17 @@ export default function Home() {
                 </Link>
               </p>
             </div>
+          </div>
+          <div className="mt-4">
+            {" "}
+            {/* Adjust margin-top using TailwindCSS */}
+            <Image
+              src="/chemeleon.jpg" // Specify the path to your image
+              alt="Description of the image" // Provide a descriptive alt text
+              width={500} // Set the desired width
+              height={300} // Set the desired height
+              className="rounded-lg shadow-sm" // Apply TailwindCSS classes for styling
+            />
           </div>
         </div>
       </div>
