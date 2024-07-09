8048ea247abd88efa8ca5fcabae23d98723934eb
VishalyGinny <vishalyginny@gmail.com>
2024-07-09 17:33:59 +0800

    done for now

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -4,8 +4,8 @@ import page from "../about/page";
 import { projects } from "@/lib/projects";
 const Page = () => {
   return (
-    <div className="flex flex-col w-full min-h-screen">
-      <header className=" py-8 px-4 md:px-6">
+    <div className="flex flex-col w-full min-h-screen items-center">
+      <header className=" py-2 px-4 md:px-6">
         <div className="container max-w-5xl mx-auto">
           <h1 className="text-3xl font-bold sm:text-4xl md:text-5xl">
             My Lastest Projects
